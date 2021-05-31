df47700cf0193e0ba5570a5746f3057f39f380d3
Viktor <web.viktor.pronyakin@gmail.com>
2021-05-31 10:16:15 +0300

    Toseef changes

--- assets/podcast-9-assets/js/script.js
+++ assets/podcast-9-assets/js/script.js
@@ -1,1 +1,3 @@
-document.addEventListener("DOMContentLoaded", function (event) {});
+document.addEventListener("DOMContentLoaded", function (event) {
+  console.log(document.querySelectorAll("iframe div"));
+});
